e4b84c66b822dd9f4579d0825f497b8e0f0770e4
wonseokcho <jws020501@naver.com>
2022-05-18 13:34:55 +0900

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow;
+// include the Node.js 'path' module at the top of your file
+const path = require('path')
+
+
+// modify your existing createWindow() function
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadURL("http://localhost:3000")
+}
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
